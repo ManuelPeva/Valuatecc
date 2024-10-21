75bcf3e0e6e160dc9e68bca81d9ef0752260121d
ManuelPeva <manuel_859@outlook.es>
2024-10-21 10:56:36 -0600

    creacion de login

---
+++ valuactecc/src/Login.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+
+function Login({onLogin}) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const users = JSON.parse(localStorage.getItem('users')) || [];
+        const user = users.find(user => user.email === email && user.password === password);
+        if(user){
+            onLogin(user);
+        }else{
+            alert('Usuario o contraseña incorrectos');
+        }
+    };
+
+    return(
+        <div>
+            <h2>Iniciar Sesión</h2>
+            <form onSubmit={handleLogin}>
+                <input
+                type="email"
+                placeholder='Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                />
+                <input
+                type='email'
+                placeholder='Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                />
+                <input
+                type='password'
+                placeholder='Contraseña'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                />
+                <button type='submit'>Entrar</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
